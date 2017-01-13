8a692db5888452d013c244d17dffeb7fb3fefc89
QasimRiaz <qasim.riaz@e2esp.com>
2017-01-13 16:13:21 +0500

    Update egpl.php

--- egpl.php
+++ egpl.php
@@ -5,7 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
- * Version:           2.6
+ * Version:           2.7
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
