8f94c2e0405e9c5395920b580a9da2e9eb6b6a61
QasimRiaz <qasim.riaz@e2esp.com>
2017-02-08 15:59:14 +0500

    fixed ficon img

--- temp/cm_header.php
+++ temp/cm_header.php
@@ -6,12 +6,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>Admin Dashboard</title>
 
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.png" rel="icon" type="image/png">
-	<link href="/wp-content/plugins/EGPL/cmtemplate/img/favicon.ico" rel="shortcut icon">
+	
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
