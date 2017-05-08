fd8a8da0e69becea944ca5b5cc02a9bcd43f063c
QasimRiaz <qasim.riaz@e2esp.com>
2017-05-08 17:09:26 +0500

    fixed new realese version number

--- egpl.php
+++ egpl.php
@@ -5,7 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
- * Version:           2.19
+ * Version:           2.20
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
