d899ed8f60457b8058c8a572be7b089bb0586d49
QasimRiaz <qasim.riaz@e2esp.com>
2019-02-18 11:13:14 +0500

    Fixed this Bug released

--- egpl.php
+++ egpl.php
@@ -5,7 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
- * Version:           3.09
+ * Version:           3.10
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
