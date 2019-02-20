fb252311628153e88e32701f16fd481a416404a2
QasimRiaz <qasim.riaz@e2esp.com>
2019-02-20 10:57:37 +0500

    fixed version no

--- egpl.php
+++ egpl.php
@@ -5,7 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
- * Version:           3.10
+ * Version:           3.11
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
