d172afa6798056e1570fe75a6774f1f373504111
qasim <qasimriaz.mdk@gmail.com>
2020-07-28 16:46:04 +0500

    fixed ravenhub api issue

--- egpl.php
+++ egpl.php
@@ -5,7 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
- * Version:           4.03
+ * Version:           4.04
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
