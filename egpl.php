dd01bcbd424599e19035382f9013ff95df135d41
QasimRiaz <mqasim@e2e.com>
2021-06-24 16:27:30 +0500

    Expo-Public Floor Plan Release

--- egpl.php
+++ egpl.php
@@ -5,6 +5,7 @@
  * Plugin Name:       EGPL
  * Plugin URI:        https://github.com/QasimRiaz/EGPL
  * Description:       EGPL
+ * Version:           4.40
  * Author:            EG
  * License:           GNU General Public License v2
  * Text Domain:       EGPL
